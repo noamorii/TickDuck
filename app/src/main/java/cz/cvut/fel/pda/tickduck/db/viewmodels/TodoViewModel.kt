280d6d9a449d7066c71cc6e0a1fbf1f7c5179aa3
Tomas Palivec <palivtom@fel.cvut.cz>
2022-05-14 02:27:51 +0200

    adding user repo to todoViewModel

--- app/src/main/java/cz/cvut/fel/pda/tickduck/db/viewmodels/TodoViewModel.kt
+++ app/src/main/java/cz/cvut/fel/pda/tickduck/db/viewmodels/TodoViewModel.kt
@@ -7,17 +7,21 @@ import androidx.lifecycle.*
 import cz.cvut.fel.pda.tickduck.MainApp
 import cz.cvut.fel.pda.tickduck.db.repository.CategoryRepository
 import cz.cvut.fel.pda.tickduck.db.repository.TodoRepository
+import cz.cvut.fel.pda.tickduck.db.repository.UserRepository
 import cz.cvut.fel.pda.tickduck.model.Category
 import cz.cvut.fel.pda.tickduck.model.Todo
+import cz.cvut.fel.pda.tickduck.model.User
 import cz.cvut.fel.pda.tickduck.model.intentDTO.NewTodoDTO
 import cz.cvut.fel.pda.tickduck.utils.SharedPreferencesKeys.CURRENT_USER_ID
 import cz.cvut.fel.pda.tickduck.utils.SharedPreferencesKeys.CURRENT_USER_PREFERENCES
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.lang.IllegalArgumentException
 
 class TodoViewModel(
     private val todoRepository: TodoRepository,
     private val categoryRepository: CategoryRepository,
+    private val userRepository: UserRepository,
     loggedInSharedPreferences: SharedPreferences
 ) : ViewModel() {
 
@@ -25,6 +29,16 @@ class TodoViewModel(
 
     val allCategoriesLiveData = categoryRepository.getAll(loggedInUserId).asLiveData()
     val allTodosLiveData = todoRepository.getAll(loggedInUserId).asLiveData()
+
+    var loggedUser: User? = null
+    init {
+        val loggedInUserId = loggedInSharedPreferences.getInt(CURRENT_USER_ID, 0)
+        if (loggedInUserId != 0) {
+            runBlocking {
+                loggedUser = userRepository.getByUserId(loggedInUserId)
+            }
+        }
+    }
 
     fun insertTodo(newTodoDTO: NewTodoDTO) = viewModelScope.launch {
         val newTodo = Todo(
@@ -68,6 +82,7 @@ class TodoViewModel(
                 return TodoViewModel(
                     TodoRepository(mainApp.todoDao),
                     CategoryRepository(mainApp.categoryDao),
+                    UserRepository(mainApp.userDao),
                     context.getSharedPreferences(CURRENT_USER_PREFERENCES, AppCompatActivity.MODE_PRIVATE)
                 ) as T
             }
